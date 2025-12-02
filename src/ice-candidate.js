ea9ce0ff0f361391b4710ad119ef0fd9f5e3415b
Roshanchau <roshanchau001@gmail.com>
2025-12-02 11:58:11 +0545

    update

--- src/ice-candidate.js
+++ src/ice-candidate.js
@@ -9,19 +9,9 @@ export const ICE_SERVERS = {
       credential: "lFvlKr/ArCoGHPzO",
     },
     {
-      urls: "turn:global.relay.metered.ca:80?transport=tcp",
-      username: "d0dc73ffd06b193670acdc79",
-      credential: "lFvlKr/ArCoGHPzO",
-    },
-    {
       urls: "turn:global.relay.metered.ca:443",
       username: "d0dc73ffd06b193670acdc79",
       credential: "lFvlKr/ArCoGHPzO",
-    },
-    {
-      urls: "turns:global.relay.metered.ca:443?transport=tcp",
-      username: "d0dc73ffd06b193670acdc79",
-      credential: "lFvlKr/ArCoGHPzO",
-    },
+    }
   ],
 };
